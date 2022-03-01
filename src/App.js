be67431b6183a0afc22a9f012e558bd29007123a
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-01 12:29:34 +0300

    add project finito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from "react";
+import Nav from "./Nav";
 import Restaurant from './Restaurant'
 import './App.css';
 
 function App() {
+const [restaurants, setRestaurants] = useState([]);
+
+
   return (
     <div className="restaurants">
-    <Restaurant/>
+      <Nav restaurants={restaurants} setRestaurants={setRestaurants}/>
+      {restaurants.map(rt => <Restaurant key={rt.id} restaurant={rt}/> )}
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Nav() {
+function Nav({restaurants,setRestaurants}) {
+ 
+const fetchRestaurant = async () => {
+    const res = await fetch("https://random-data-api.com/api/restaurant/random_restaurant")
+    const data = await res.json();
+    setRestaurants([...restaurants,data]);
+}
+
+
   return (
     <div className='navbar'>
-        <button>Add new </button>
+        <button onClick ={fetchRestaurant}>Add New Restaurant </button>
     </div>
   )
 }
